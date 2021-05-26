40620ccaec4ae3236df18575fee7c75d12b4e46a
Stone <byxovskiy93@mail.ru>
2021-05-26 11:46:47 +0300

    add article api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
+    Route::post('article/save', 'ArticleController@store');
+    Route::get('article/list', 'ArticleController@index');
 });
+
